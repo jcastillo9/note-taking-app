9c2be702d473e32b5da4ccd499b09eb1b9534a0b
Janette <janette.castillo91@yahoo.com>
2021-12-11 21:18:04 -0600

    added GET routes

--- server.js
+++ server.js
@@ -10,3 +10,11 @@ app.use(express.static('public'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
